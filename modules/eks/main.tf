0d73322c115b8c28ecc6f92df0051c2c0158d750
Ignatus <ignatusa3@gmail.com>
2025-04-13 00:32:42 +0000

    update kubernetes.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -4,10 +4,10 @@ resource "aws_eks_cluster" "eks_cluster" {
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = var.public_subnet_ids
+    subnet_ids              = var.public_subnet_ids
     endpoint_private_access = true
     endpoint_public_access  = true
-    security_group_ids = [var.cluster_security_group_id] 
+    security_group_ids      = [var.cluster_security_group_id]
   }
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
@@ -16,24 +16,26 @@ resource "aws_eks_cluster" "eks_cluster" {
 
 
 
-resource "aws_eks_node_group" "eks_node_group" {
+resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = var.node_group_name
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = var.public_subnet_ids
-  instance_types  = [var.node_group_instance_type]
+  subnet_ids      = var.private_subnet_ids
 
   scaling_config {
     desired_size = var.desired_capacity
-    min_size     = var.min_capacity
     max_size     = var.max_capacity
+    min_size     = var.min_capacity
   }
+
+  instance_types = [var.node_group_instance_type]
 
   depends_on = [
     aws_iam_role_policy_attachment.eks_node_policy,
     aws_iam_role_policy_attachment.eks_cni_policy,
-    aws_iam_role_policy_attachment.eks_registry_policy,
-    aws_eks_cluster.eks_cluster
+    aws_iam_role_policy_attachment.eks_container_registry_policy
   ]
+
+  tags = var.tags
 }
 
--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,55 +1,55 @@
-# # EC2 Security Group
-# resource "aws_security_group" "this" {
-#   name   = "${var.project_name}-${var.environment}-sg"
-#   vpc_id = var.vpc_id
+# EC2 Security Group
+resource "aws_security_group" "this" {
+  name   = "${var.project_name}-${var.environment}-sg"
+  vpc_id = var.vpc_id
 
-#   ingress = [
-#     {
-#       from_port        = 22
-#       to_port          = 22
-#       protocol         = "tcp"
-#       cidr_blocks      = ["0.0.0.0/0"]
-#       description      = "SSH"
-#       ipv6_cidr_blocks = []
-#       prefix_list_ids  = []
-#       security_groups  = []
-#       self            = false
-#     },
-#     {
-#       from_port        = 443
-#       to_port          = 443
-#       protocol         = "tcp"
-#       cidr_blocks      = ["0.0.0.0/0"]
-#       description      = "HTTPS"
-#       ipv6_cidr_blocks = []
-#       prefix_list_ids  = []
-#       security_groups  = []
-#       self            = false
-#     },
-#     {
-#       from_port        = 4000
-#       to_port          = 4000
-#       protocol         = "tcp"
-#       cidr_blocks      = ["0.0.0.0/0"]
-#       description      = "Node.js App"
-#       ipv6_cidr_blocks = []
-#       prefix_list_ids  = []
-#       security_groups  = []
-#       self            = false
-#     }
-#   ]
-#   egress = [{
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     description      = "All"
-#     ipv6_cidr_blocks = []
-#     prefix_list_ids  = []
-#     security_groups  = []
-#     self            = false
-#   }]
-# }
+  ingress = [
+    {
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "SSH"
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self            = false
+    },
+    {
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "HTTPS"
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self            = false
+    },
+    {
+      from_port        = 3000
+      to_port          = 3000
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "Node.js App"
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self            = false
+    }
+  ]
+  egress = [{
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "All"
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self            = false
+  }]
+}
 
 # EKS Cluster Security Group
 resource "aws_security_group" "eks_cluster" {
